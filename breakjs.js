e4c5ad50990bd558623793d0b2374c7a99fc5429
clausaji <c.sanchezjimenez@qquest.nl>
2022-07-27 09:30:23 +0200

    All the files so far

---
+++ breakjs.js
@@ -0,0 +1,216 @@
+let board = document.querySelector("#board");
+let blocks = new Array();
+const visualBlocks = new Array();
+const user = document.createElement("div");
+const ball = document.createElement("div");
+const startingPosition = [40, 10];
+let currentPosition = startingPosition;
+document.addEventListener("keydown", (e) => {
+    moveUser(e)
+});
+const startingPBall = [90, 40];
+let currentPBall = startingPBall;
+let gameStarted = false;
+
+
+
+class Block{
+    constructor(x, y){
+        this.left = x;
+        this.leftBottom = y + 20;
+        this.top = y;
+        this.rightCorner = x + 60;
+        this.bottom = 480 - y - 20 - 10;
+    }
+}
+
+populateTheBoard();
+
+function populateTheBoard(){
+    for (i = 0; i < 5; i++){
+        for (x = 0; x < 10; x++){
+        let block = new Block(10 + 65 * x, 10 + 30 * i);
+        blocks.push(block);
+        }
+    }
+
+    for (i = 0; i < 50; i++){
+        let block = document.createElement("div");
+        block.classList.add("block");
+        block.style.left = blocks[i].left + "px";
+        block.style.top = blocks[i].top + "px";
+        block.setAttribute("id", i);
+        visualBlocks.push(block);
+    
+        board.appendChild(block);
+    }
+
+    user.classList.add("user");
+    board.appendChild(user);
+    refreshUserPosition();
+
+    ball.classList.add("ball");
+    board.appendChild(ball);
+    refreshBallPosition();
+}
+
+function refreshUserPosition(){
+    user.style.left = currentPosition[0] + "px";
+    user.style.bottom = currentPosition[1] + "px";
+}
+
+function refreshBallPosition(){
+    ball.style.left = currentPBall[0] + "px";
+    ball.style.bottom = currentPBall[1] + "px";
+}
+
+function moveUser(e){
+    switch(e.key){
+        case 'ArrowLeft': 
+            if (!gameStarted){
+                moveTheBallDown(0);
+                gameStarted = true;
+                return gameStarted;
+            }
+            if(currentPosition[0] > 10){
+                currentPosition[0] -= 15;
+                refreshUserPosition();
+            } else if (currentPosition[0] >= 0){
+                currentPosition[0] = 0;
+                refreshUserPosition();
+            }
+            break;
+        case 'ArrowRight': 
+            if (!gameStarted){
+                moveTheBallDown(0);
+                gameStarted = true;
+                return gameStarted;
+            }
+            if(currentPosition[0] < 560){
+                currentPosition[0] += 15;
+                refreshUserPosition();
+            } else if (currentPosition[0] > 560){
+                currentPosition[0] = 564;
+                refreshUserPosition();
+            }
+            break;
+    }
+
+}
+
+function moveTheBallDown(num){
+    if (checkForWin()){
+        document.querySelector("h1").innerHTML = "Congrats you won! 😃"
+        return;
+        }
+    currentPBall[0] += num;
+    currentPBall[1] -= 5;
+    refreshBallPosition();
+    if (currentPBall[1] == 0){
+        document.querySelector("h1").innerHTML = "You lost! 😣";
+        return;
+    } else if(currentPBall[1] == 30 && currentPBall[0] >= currentPosition[0] - 5 && currentPBall[0] <= currentPosition[0] + 105){
+        if(currentPBall[0] >= currentPosition[0] + 40 && currentPBall[0] <= currentPosition[0] + 100 - 40){
+            setTimeout(moveTheBallUp, 20, 0);
+            return;
+        } else if(currentPBall[0] > currentPosition[0] - 5 && currentPBall[0] < currentPosition[0] + 100 - 60){
+            setTimeout(moveTheBallUp, 20, 5);
+            return;
+        } else if(currentPBall[0] > currentPosition[0] + 60 && currentPBall[0] < currentPosition[0] + 105){
+            setTimeout(moveTheBallUp, 20, -5);
+            return;
+        }
+
+    } else if(currentPBall[1] < 30 && currentPBall[1] > 10) {
+        if  (currentPBall[0] == currentPosition[0]) {
+            setTimeout(moveTheBallUp, 20, -5);
+            return;
+        } else if(currentPBall[0] == currentPosition[0] + 105) {
+            setTimeout(moveTheBallUp, 20, 5);
+            return;
+        }
+        
+    }
+    
+    for(i = 0; i < blocks.length; i++){
+        if (currentPBall[1] >= blocks[i].bottom && currentPBall[1] <= blocks[i].bottom + 25 && currentPBall[0] == blocks[i].left){
+            hitBlock(i);
+            setTimeout(moveTheBallDown, 20, -5);
+            return;
+        } else if(currentPBall[1] >= blocks[i].bottom && currentPBall[1] <= blocks[i].bottom + 25 && currentPBall[0] == blocks[i].rightCorner){
+            hitBlock(i);
+            setTimeout(moveTheBallDown, 20, 5);
+            return;
+        } else if  (currentPBall[1] == blocks[i].bottom + 20 && currentPBall[0] >= blocks[i].left -5 && currentPBall[0] <= blocks[i].rightCorner) {
+            hitBlock(i);
+            setTimeout(moveTheBallUp, 20, num);
+            return;
+        }
+    } 
+    
+    if (currentPBall[0] == 0){
+        setTimeout(moveTheBallDown, 20, 5);
+        return;
+    } else if (currentPBall[0] == 655){
+        setTimeout(moveTheBallDown, 20, -5);
+        return;
+    } else {
+        setTimeout(moveTheBallDown, 20, num);
+    }
+}
+
+function moveTheBallUp(num){
+    if (checkForWin()){
+        document.querySelector("h1").innerHTML = "Congrats you won! 😃"
+        return;
+        }
+
+    currentPBall[0] += num;
+    currentPBall[1] += 5;
+    refreshBallPosition();
+    for(i = 0; i < blocks.length; i ++){
+        if (currentPBall[1] == blocks[i].bottom && currentPBall[0] >= blocks[i].left -5 && currentPBall[0] <= blocks[i].rightCorner){
+            hitBlock(i);
+            setTimeout(moveTheBallDown, 20, num);
+            return;
+        } else if(currentPBall[1] >= blocks[i].bottom && currentPBall[1] <= blocks[i].bottom + 25 && currentPBall[0] == blocks[i].left){
+            hitBlock(i);
+            setTimeout(moveTheBallUp, 20, -5);
+            return;
+        } else if(currentPBall[1] >= blocks[i].bottom && currentPBall[1] <= blocks[i].bottom + 25 && currentPBall[0] == blocks[i].rightCorner){
+            hitBlock(i);
+            setTimeout(moveTheBallUp, 20, 5);
+            return;
+        }
+    } 
+
+    if (currentPBall[0] == 0){
+        setTimeout(moveTheBallUp, 20, 5);
+        return;
+    } else if (currentPBall[0] == 655){
+        setTimeout(moveTheBallUp, 20, -5);
+        return;
+    } else if (currentPBall[1] == 470){
+        setTimeout(moveTheBallDown, 20, num);       
+    } else {
+        setTimeout(moveTheBallUp, 20, num);
+    }
+
+
+}
+
+function hitBlock(i){
+    if(document.getElementById(i).style.backgroundColor != "rgb(0, 255, 255)"){
+        document.getElementById(i).style.backgroundColor = "#00FFFF";
+    } else {
+        document.getElementById(i).remove();
+        visualBlocks.splice(i, 1);
+        blocks[i].bottom = 0;
+        blocks[i].left = 0;
+       
+    }
+}
+
+function checkForWin(){
+    return visualBlocks.length == 0;
+}
